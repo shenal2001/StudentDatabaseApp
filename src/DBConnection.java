9787c21c0b3cc22b839f3f01e99949c7e1162814
shenal2001 <149025975+shenal2001@users.noreply.github.com>
2025-09-02 12:04:49 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
     
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "70329818sS#";
+    private static final String PASSWORD = "2021s19033";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
